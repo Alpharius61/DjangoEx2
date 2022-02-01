74b51d96025801c6bac026096e6c5ed65652b43f
Alpharius61 <monlouis.gael@gmail.com>
2022-02-01 11:26:53 +0100

    up

---
+++ peopleList/people/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class people(models.Model):
+
+    name = models.CharField(max_length=50, null=False)
+    Sexe = models.CharField(max_length=1, null=False)
